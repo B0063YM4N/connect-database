c1e53d387237b84d194de6608a20bb2632a7f673
B0063YM4N <gogatadzebeka@gmail.com>
2023-11-20 22:52:50 +0400

    test commit

--- src/app/api/data/route.js
+++ src/app/api/data/route.js
@@ -2,8 +2,37 @@ import { NextResponse } from "next/server";
 import data from "../../../../data";
 
 export const GET = async (req) => {
-    
-      return new NextResponse(JSON.stringify(data), { status: 201 });
+  const dataA = {
+    message: "ok",
+    data: [
+      {
+        id: 1,
+        nameGe: "რეგულარი",
+        nameEn: "Regular",
+        price: 2.89,
+      },
+      {
+        id: 2,
+        nameGe: "პრემიუმი",
+        nameEn: "Premium",
+        price:3.04,
+      },
+      {
+        id: 3,
+        nameGe: "დიზელი",
+        nameEn: "Diesel",
+        price: 3.04,
+      },
+      {
+        id: 4,
+        nameGe: "ევრო დიზელი",
+        nameEn: "Euro Diesel",
+        price: 3.19,
+      },
+      
+    ],
+  };
+  return new NextResponse(JSON.stringify(dataA), { status: 201 });
 }
 
 
@@ -18,5 +47,5 @@ export const POST = async (req) => {
   }
   
 
-  return new NextResponse("works", { status: 201 });
+  return new NextResponse(JSON.stringify(data), { status: 201 });
 }
